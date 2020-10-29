e195102e84290bf803050f8af297c49a201c2593
Tiago Cunha <tiagotcunha@gmail.com>
2020-10-29 03:50:06 +0000

    setup router

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import "./App.css";
 import ScreenLoader from "./pages/screen_loading";
 import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import ScreenList from "./pages/screen_list";
+import ScreenDetail from "./pages/screen_detail";
 const AppHeader = styled.header`
   background: red;
 `;
 function App() {
   return (
     <div>
-      <Router>
-        <AppHeader />
-        <Switch>
-          <Route exact path="/">
-            <ScreenLoader />
-          </Route>
-          {/* <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route> */}
-        </Switch>
-      </Router>
+      <ThemeProvider >
+        <CSSReset />
+        <Router>
+          <AppHeader />
+          <Switch>
+            <Route exact path="/">
+              <ScreenLoader />
+            </Route>
+            <Route path="/list">
+              <ScreenList />
+            </Route>
+            <Route path="/question/:id">
+              <ScreenDetail />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
