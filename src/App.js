46d99354756adbdb82b6d1a6186ee7c5ba2cff09
Tiago Cunha <tiagotcunha@gmail.com>
2020-10-29 02:04:48 +0000

    add chakra ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
-import ScreenLoader from './pages/screen_loading'
+import "./App.css";
+import ScreenLoader from "./pages/screen_loading";
+import styled from "styled-components";
+
+const AppHeader = styled.header`
+  background: red;
+`;
 function App() {
   return (
     <div>
+      <AppHeader />
       <ScreenLoader />
     </div>
   );
