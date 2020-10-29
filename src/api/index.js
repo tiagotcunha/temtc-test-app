fd498e0cfc18e89ccc6a4003901595e6cf9d4760
Tiago Cunha <tiagotcunha@gmail.com>
2020-10-29 01:37:04 +0000

    setup api

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_API}`,
+});
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    console.log(error);
+    return Promise.reject(new Error(error));
+  }
+);
+
+export const fetchStatus = async () => {
+    return await instance.get('/health');
+}
+
+export const fetchList = async () => {
+    return await instance.get('/questions',{
+        params: {
+            limit:"",
+            offset:"",
+            filter:""
+        }
+    });
+}
+
+
+export const fetchQuestion = async (id) => {
+    return await instance.get('/questions/'+ id);
+}
+
+
+export const updateQuestion = async (id) => {
+    return await instance.put('/questions/'+ id);
+}
+
